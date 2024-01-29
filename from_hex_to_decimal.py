5ef8fe27aacbfb37ed11188df602b867b08fa19f
georgetoloraia <gtoloraia19@gmail.com>
2024-01-29 16:05:02 +0400

    add from_hex_to_decimal.py - [] this code can convert hex number to decimal number

---
+++ from_hex_to_decimal.py
@@ -0,0 +1,4 @@
+# Convert the hexadecimal number to decimal
+hex_number = "30210c23b1a047bc9bdbb13448e67deddc108946de6de639bcc75d47c0216b1b"
+decimal_number = int(hex_number, 16)
+print(decimal_number)
